f3a1ab169b97ff66691108f5de1e3782246c27bc
Sebastian Greenholtz <smgreenholtz@gmail.com>
2016-06-04 11:11:27 -0500

    Added Search servlet and db handler object

---
+++ project/src/dbOperations/SearchHandler.java
@@ -0,0 +1,39 @@
+package dbOperations;
+
+import java.sql.*;
+import java.util.*;
+
+/**
+ * Performs searches on the database to find events.
+ * @author Sebastian Greenholtz
+ */
+
+public class SearchHandler extends DatabaseHandler {
+
+    private Connection conn;
+
+    /**
+     * Empty constructor
+     */
+    public SearchHandler() {
+
+    }
+
+    /**
+     * Constructor with properties variable
+     * @param properties Application properties
+     */
+    public SearchHandler(Properties properties) {
+        super(properties);
+        conn = getConnection();
+        System.out.println("***** DATABASE CONNECTION: " + conn);
+    }
+
+    /**
+     * Parses through the location search to determine what kind of search to perform
+     * @param location Search string from the location field
+     */
+    public void parseLocationSearch(String location) {
+        if (location.matches())
+    }
+}
--- project/src/servlets/SearchServlet.java
+++ project/src/servlets/SearchServlet.java
@@ -1,14 +1,38 @@
 package servlets;
 
+import dbOperations.SearchHandler;
+
 import java.io.*;
+import java.util.Properties;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
 /**
+ * This is the servlet for performing a search. Takes in the search data and sends to a view
+ * of search results.
+ *
  * @author Sebastian Greenholtz
  */
 
 public class SearchServlet extends HttpServlet {
 
+    /**
+     * Gets the search terms and performs a search in the database
+     *
+     * @param request Http Request
+     * @param response Http Response
+     * @throws IOException
+     * @throws ServletException
+     */
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
 
+        String location = request.getParameter("location");
+        String keyword = request.getParameter("keyword");
+
+        Properties properties = (Properties) getServletContext().getAttribute("appProperties");
+        SearchHandler searcher = new SearchHandler(properties);
+
+
+    }
 }
