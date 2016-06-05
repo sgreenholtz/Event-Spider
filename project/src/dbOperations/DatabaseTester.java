191e4a3fa443f523545b93204b921f4716c3a370
Sebastian Greenholtz <smgreenholtz@gmail.com>
2016-06-05 11:17:37 -0500

    Creating Search function, title only to start

--- project/src/dbOperations/DatabaseTester.java
+++ project/src/dbOperations/DatabaseTester.java
@@ -1,14 +1,20 @@
 package dbOperations;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * Class to test database connection
  * @author Sebastian Greenholtz
  */
 public class DatabaseTester {
 
-    public static void main(String[] args) {
-        DatabaseHandler handler = new DatabaseHandler("root", "student",
-                "jdbc:mysql://localhost:3306/eventspider?useSSL=false");
-        System.out.println(handler.getConnection());
+    public static void main(String[] args) throws SQLException {
+        String search = "Sample event";
+        SearchHandler searcher = new SearchHandler("root", "student", "jdbc:mysql://localhost:3306/eventspider?useSSL=false");
+        ResultSet results = searcher.performTitleSearch(search);
+        while (results.next()) {
+            System.out.println(results.getString("title"));
+        }
     }
 }
--- project/src/dbOperations/SearchHandler.java
+++ project/src/dbOperations/SearchHandler.java
@@ -29,10 +29,66 @@ public class SearchHandler extends DatabaseHandler {
     }
 
     /**
-     * Parses through the location search to determine what kind of search to perform
-     * @param location Search string from the location field
+     * Constructor using strings for username, password and url, for use in
+     * testing only
+     * @param username Database username
+     * @param password Database password
+     * @param url Database url
      */
-    public void parseLocationSearch(String location) {
-//        if (location.matches())
+    public SearchHandler(String username, String password, String url) {
+        super(username, password, url);
+        conn = getConnection();
+    }
+
+    /**
+     * Performs the search on the database
+     * @param searchString String from the Keyword field on the search form
+     * @return Results Set of the search
+     */
+    public ResultSet performTitleSearch (String searchString) {
+        ResultSet results = null;
+        PreparedStatement statement = createSearchStatementForTitle(splitSearchStringIntoTokens(searchString));
+        try {
+            results = statement.executeQuery();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return results;
+    }
+
+    /**
+     * Creates an ArrayList of individual words to search the database on
+     * @param searchString String from the keyword field of the search form
+     * @return Array List of search terms
+     */
+    private ArrayList<String> splitSearchStringIntoTokens(String searchString) {
+        ArrayList<String> searchTerms = new ArrayList<>();
+        for (String term : searchString.split("\\W")) {
+            if (!(term.isEmpty())) {
+                searchTerms.add(term);
+            }
+        }
+        return searchTerms;
+    }
+
+    /**
+     * Creates a Prepared Statement from the array list of search terms, searching the Title field of the database
+     * @param searchTerms Array List of words to search on
+     * @return a Prepared Statement for performing the search
+     */
+    private PreparedStatement createSearchStatementForTitle(ArrayList<String> searchTerms) {
+        PreparedStatement statement = null;
+        String sql = "SELECT * FROM Events WHERE title LIKE ";
+        for (int i=0; i<searchTerms.size()-1; i++) {
+            sql += "'%" + searchTerms.get(i) + "%' OR title LIKE ";
+        }
+        sql+= "'%" + searchTerms.get(searchTerms.size()-1) + "%'";
+        try {
+            statement = conn.prepareStatement(sql);
+            System.out.println(statement);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return statement;
     }
 }
