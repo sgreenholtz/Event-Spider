d8527bc6c2495084c57c9da1743d3524c7a123fc
Sebastian Greenholtz <smgreenholtz@gmail.com>
2016-06-26 12:03:51 -0500

    Setting up Lucene application

---
+++ Lucene/src/Practice/LuceneTester.java
@@ -0,0 +1,7 @@
+package Practice;
+
+/**
+ * @author Sebastian Greenholtz
+ */
+public class LuceneTester {
+}
---
+++ Lucene/src/Practice/TextFileFilter.java
@@ -0,0 +1,15 @@
+package Practice;
+
+import java.io.*;
+
+/**
+ * A Txt file filter from Tutorialpoint
+ * @author Sebastian Greenholtz
+ */
+public class TextFileFilter implements FileFilter {
+
+    @Override
+    public boolean accept(File pathname) {
+        return pathname.getName().toLowerCase().endsWith(".txt");
+    }
+}
