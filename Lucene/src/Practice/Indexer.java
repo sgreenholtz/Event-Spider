bf4d6b43c402748f56d7e12af4de92f541ee05eb
Sebastian Greenholtz <smgreenholtz@gmail.com>
2016-06-28 20:45:51 -0500

    Creating Lucene index

--- Lucene/src/Practice/Indexer.java
+++ Lucene/src/Practice/Indexer.java
@@ -1,82 +1,90 @@
 package Practice;
 
 import java.io.*;
+import java.nio.file.Paths;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-//import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
+import org.apache.lucene.analysis.en.*;
+import org.apache.lucene.document.*;
+import org.apache.lucene.store.*;
+
 import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.util.Version;
+import org.apache.lucene.index.IndexWriterConfig;
+
 
 /**
- * Indexes raw data for search using code from Tutorialspoint
+ * Indexes database data, based on Tutorialspoint code but heavily modified.
  * @author Sebastian Greenholtz
  */
 public class Indexer {
 
     private IndexWriter writer;
 
-    public Indexer(String indexDirectoryPath) throws IOException{
-        //this directory will contain the indexes
-        Directory indexDirectory =
-                FSDirectory.open(new File(indexDirectoryPath));
-
-        //create the indexer
-        writer = new IndexWriter(indexDirectory,
-                new StandardAnalyzer(Version.LUCENE_36),true,
-                IndexWriter.MaxFieldLength.UNLIMITED);
+    /**
+     * Constructor creates new Indexer with a simple English Analyzer
+     * using the given path as the directory to store indexes
+     * @param indexDirectoryPath Path to the directory where indexes should be stored
+     * @throws IOException
+     */
+    public Indexer(String indexDirectoryPath) throws IOException {
+        Directory indexDirectory = FSDirectory.open(Paths.get(indexDirectoryPath));
+        writer = new IndexWriter(indexDirectory, new IndexWriterConfig(new EnglishAnalyzer()));
     }
 
+    /**
+     * Closes the IndexWriter
+     * @throws CorruptIndexException
+     * @throws IOException
+     */
     public void close() throws CorruptIndexException, IOException{
         writer.close();
     }
 
-    private Document getDocument(File file) throws IOException{
+    /**
+     * Creates a document with the ID and Title fields
+     * @param id event_id field, Integer
+     * @param title title field, String
+     * @return Document of fields
+     * @throws IOException
+     */
+    private Document getDocument(Integer id, String title) throws IOException {
         Document document = new Document();
-
-        //index file contents
-        Field contentField = new Field(LuceneConstants.CONTENTS,
-                new FileReader(file));
-        //index file name
-        Field fileNameField = new Field(LuceneConstants.FILE_NAME,
-                file.getName(),
-                Field.Store.YES,Field.Index.NOT_ANALYZED);
-        //index file path
-        Field filePathField = new Field(LuceneConstants.FILE_PATH,
-                file.getCanonicalPath(),
-                Field.Store.YES,Field.Index.NOT_ANALYZED);
-
-        document.add(contentField);
-        document.add(fileNameField);
-        document.add(filePathField);
-
+        document.add(new StoredField("event_id", id));
+        document.add(new TextField("title", title, Field.Store.YES));
         return document;
     }
 
-    private void indexFile(File file) throws IOException{
-        System.out.println("Indexing "+file.getCanonicalPath());
-        Document document = getDocument(file);
-        writer.addDocument(document);
-    }
-
-    public int createIndex(String dataDirPath, FileFilter filter)
-            throws IOException{
-        //get all files in the data directory
-        File[] files = new File(dataDirPath).listFiles();
-
-        for (File file : files) {
-            if(!file.isDirectory()
-                    && !file.isHidden()
-                    && file.exists()
-                    && file.canRead()
-                    && filter.accept(file)
-                    ){
-                indexFile(file);
-            }
+    /**
+     * Indexes the Result Set from a database query using the Event ID and the
+     * Title fields
+     * @param results Result Set from the database Select query
+     * @throws IOException
+     * @throws SQLException
+     */
+    private void indexResultSet(ResultSet results) throws IOException, SQLException {
+        while (results.next()) {
+            Document document = getDocument(results.getInt("event_id"), results.getString("title"));
+            writer.addDocument(document);
         }
-        return writer.numDocs();
     }
+
+//    public int createIndex(String dataDirPath, FileFilter filter)
+//            throws IOException{
+//        //get all files in the data directory
+//        File[] files = new File(dataDirPath).listFiles();
+//
+//        for (File file : files) {
+//            if(!file.isDirectory()
+//                    && !file.isHidden()
+//                    && file.exists()
+//                    && file.canRead()
+//                    && filter.accept(file)
+//                    ){
+//                indexFile(file);
+//            }
+//        }
+//        return writer.numDocs();
+//    }
 }
