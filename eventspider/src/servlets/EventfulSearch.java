882569f3d827e3cb16ad28cc0cda7d0e17b08091
Sebastian Greenholtz <smgreenholtz@gmail.com>
2016-07-03 10:34:54 -0500

    For some reason my changes to the web app aren't being posted to Tomcat

--- eventspider/src/servlets/EventfulSearch.java
+++ eventspider/src/servlets/EventfulSearch.java
@@ -20,9 +20,20 @@ public class EventfulSearch extends HttpServlet {
      * @param request
      * @param response
      */
-    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException {
         properties = (Properties) getServletContext().getAttribute("appProperties");
+        String searchUrl = constructURL(request.getParameter("location"));
 
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<p>" + searchUrl + "</p>");
+        out.println("</body>");
+        out.println("</html>");
     }
 
     /**
@@ -33,8 +44,22 @@ public class EventfulSearch extends HttpServlet {
     private String constructURL(String location) {
         String url = "http://api.eventful.com/json/events/search?"; //kvxN9gH3zxsQhvCF&location=Madison";
         url += "app_key=" + properties.getProperty("eventful.api");
-        url += "&location=" + location;
+        url += "&location=" + constructSearchString(location);
         return url;
+    }
+
+    /**
+     * Constructs a string that is safe for passing through a URL by taking out non word
+     * characters and replacing them with a + sign
+     * @param field Field to convert into a URL safe form
+     * @return URL safe String of field
+     */
+    private String constructSearchString(String field) {
+        String urlSafeString = "";
+        for (String token : field.split("\\W")) {
+            urlSafeString += token + "+";
+        }
+        return urlSafeString.substring(0, urlSafeString.length()-1);
     }
 
 }
