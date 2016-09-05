66f7270e89a0852289b1b58ddb892f9b1bff6da4
Sebastian Greenholtz <smgreenholtz@gmail.com>
2016-09-05 10:46:43 -0500

    Added Application startup controller

---
+++ eventspider/src/main/java/controllers/Application.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Application startup controller
+ * @author Sebastian Greenholtz
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
