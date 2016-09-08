78978736e49b2b2724b7e812bf8613879676a5e0
Sebastian Greenholtz <smgreenholtz@gmail.com>
2016-09-07 19:28:58 -0500

    Adding Log4J

--- eventspider/src/main/java/controllers/LoginController.java
+++ eventspider/src/main/java/controllers/LoginController.java
@@ -1,6 +1,9 @@
 package controllers;
 
+import beans.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,9 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
 
-    @RequestMapping("/login")
-    public String login() {
-
+    @PostMapping("/login")
+    public String login(@ModelAttribute User user) {
+        return "result";
     }
 }
 /*
