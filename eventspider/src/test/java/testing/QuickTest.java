71445e8a40a39ff6b7b803d1882b86613e12f2f5
Sebastian Greenholtz <smgreenholtz@gmail.com>
2016-09-14 17:38:26 -0500

    Fixed .gitignore in repo

---
+++ eventspider/src/test/java/testing/QuickTest.java
@@ -0,0 +1,17 @@
+package testing;
+
+import java.text.*;
+import java.util.*;
+
+/**
+ * Quick testing in command line
+ * @author Sebastian Greenholtz
+ */
+public class QuickTest {
+
+    public static void main(String[] args) throws ParseException {
+        new Integer(1094222999);
+
+    }
+}
+
