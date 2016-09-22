fdd4b8d486af818f3333b184481e412742f67483
Sebastian Greenholtz <smgreenholtz@gmail.com>
2016-09-21 19:01:34 -0500

    Changes

--- eventspider/src/main/java/eventspider/beans/User.java
+++ eventspider/src/main/java/eventspider/beans/User.java
@@ -1,13 +1,29 @@
 package eventspider.beans;
 
+import org.hibernate.annotations.Table;
+
+import javax.persistence.*;
+
 /**
  * Represents a user
  * @author Sebastian Greenholtz
  */
+@Entity
+@Table(name = "Users")
 public class User {
-
+    @Id
+    @Column(name = "user_id")
     private int userID;
-    private String username;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "pass")
+    private String pass;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "role")
+    private String role;
 
     /**
      * Gets the value of userID.
@@ -29,15 +45,15 @@ public class User {
      * Gets the value of username.
      * @return username
      */
-    public String getUsername() {
-        return username;
+    public String getEmail() {
+        return email;
     }
 
     /**
      * Sets username to given value
-     * @param username value to set instance variable to
+     * @param email value to set instance variable to
      */
-    public void setUsername(String username) {
-        this.username = username;
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
