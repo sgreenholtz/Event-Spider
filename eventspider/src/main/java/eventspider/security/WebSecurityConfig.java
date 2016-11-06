c60468384306785444a7e85b6fba8aec68a7ce3d
Sebastian Greenholtz <smgreenholtz@gmail.com>
2016-11-06 16:34:18 -0600

    Log in is being request on homepage

--- eventspider/src/main/java/eventspider/security/WebSecurityConfig.java
+++ eventspider/src/main/java/eventspider/security/WebSecurityConfig.java
@@ -19,7 +19,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/search", "/searchResult", "/event-details   ").permitAll()
+                    .antMatchers("", "/search", "/searchResult", "/event-details   ").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
