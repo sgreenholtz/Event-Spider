1af62fde47690a9de1b9093564615e1409e37804
Sebastian Greenholtz <smgreenholtz@gmail.com>
2016-11-13 15:35:50 -0600

    Changes

--- eventspider/src/main/java/eventspider/security/WebSecurityConfig.java
+++ eventspider/src/main/java/eventspider/security/WebSecurityConfig.java
@@ -1,38 +1,38 @@
-package eventspider.security;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-/**
- * Configurations for Spring Security, from <a href="https://spring.io/guides/gs/securing-web/">this tutorial</a>
- * @author Sebastian Greenholtz
- */
-
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/", "index", "/search/**", "/register").permitAll()
-                .antMatchers("/user/**").hasRole("MEMBER")
-                .antMatchers("/admin").hasRole("ADMIN")
-                .and()
-                .formLogin()
-                .loginPage("/login").failureUrl("/error");
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("user").password("password").roles("MEMBER");
-    }
-
-}
+//package eventspider.security;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//
+///**
+// * Configurations for Spring Security, from <a href="https://spring.io/guides/gs/securing-web/">this tutorial</a>
+// * @author Sebastian Greenholtz
+// */
+//
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/", "index", "/search/**", "/register").permitAll()
+//                .antMatchers("/user/**").hasRole("MEMBER")
+//                .antMatchers("/admin").hasRole("ADMIN")
+//                .and()
+//                .formLogin()
+//                .loginPage("/login").failureUrl("/error");
+//    }
+//
+////    @Autowired
+////    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+////        auth
+////                .inMemoryAuthentication()
+////                .withUser("user").password("password").roles("MEMBER");
+////    }
+//
+//}
