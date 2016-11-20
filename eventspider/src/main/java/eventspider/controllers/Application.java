e25032dfb78b3abe28fb18f70f2dd69cc08f0d51
Sebastian Greenholtz <smgreenholtz@gmail.com>
2016-11-20 11:22:32 -0600

    Added log statement for starting new instance of application

--- eventspider/src/main/java/eventspider/controllers/Application.java
+++ eventspider/src/main/java/eventspider/controllers/Application.java
@@ -3,6 +3,8 @@ package eventspider.controllers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.apache.log4j.*;
+
 /**
  * Application startup controller
  * @author Sebastian Greenholtz
@@ -10,7 +12,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class  Application {
 
+    private static final Logger log = Logger.getLogger(Application.class);
+
     public static void main(String[] args) {
+        log.info("******Starting Event Spider******");
         SpringApplication.run(Application.class, args);
     }
 
