431d9d86c630bf7242e79a77eb55177db5994ed9
Sebastian Greenholtz <smgreenholtz@gmail.com>
2016-11-20 15:29:24 -0600

    Save worked correctly

---
+++ eventspider/src/main/java/eventspider/beans/EventBeanComparator.java
@@ -0,0 +1,14 @@
+package eventspider.beans;
+
+import java.util.Comparator;
+
+/**
+ * Custom comparator for EventBean by date
+ * @author Sebastian Greenholtz
+ */
+public class EventBeanComparator implements Comparator<EventBean> {
+    @Override
+    public int compare(EventBean t1, EventBean t2) {
+        return t1.getStartDate().compareTo(t2.getStartDate());
+    }
+}
