ebc29dc36864b7d7bd88ea167cac038df2a76c6f
Sebastian Greenholtz <smgreenholtz@gmail.com>
2016-12-03 13:43:21 -0600

    Fixed constructors

--- eventspider/src/main/java/eventspider/beans/UserSavedEventsPK.java
+++ eventspider/src/main/java/eventspider/beans/UserSavedEventsPK.java
@@ -10,9 +10,18 @@ public class UserSavedEventsPK implements Serializable {
     protected Integer userID;
     protected Integer eventID;
 
+    /**
+     * Empty constructor
+     */
     public UserSavedEventsPK(){}
 
+    /**
+     * Constructor setting the instance variables
+     * @param userID
+     * @param eventID
+     */
     public UserSavedEventsPK(Integer userID, Integer eventID) {
+        this();
         this.userID = userID;
         this.eventID = eventID;
     }
